7233c3781fc97d76e99fce36c078db896d898ab2
maihongphong <maihongphong1999@gmail.com>
2021-05-25 18:05:01 +0700

    update Form1 end FormRegister

--- WF6/WF6/FormRegister.cs
+++ WF6/WF6/FormRegister.cs
@@ -46,5 +46,10 @@ namespace WF6
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- WF6/WF6/Formregister.Designer.cs
+++ WF6/WF6/Formregister.Designer.cs
@@ -43,6 +43,7 @@ namespace WF6
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -117,6 +118,7 @@ namespace WF6
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(361, 23);
             this.textBox2.TabIndex = 7;
+            this.textBox2.UseSystemPasswordChar = true;
             // 
             // textBox4
             // 
@@ -146,19 +148,20 @@ namespace WF6
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(154, 316);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(100, 316);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 33);
+            this.button1.Size = new System.Drawing.Size(95, 33);
             this.button1.TabIndex = 12;
             this.button1.Text = "Delete";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(287, 316);
+            this.button2.Location = new System.Drawing.Point(215, 316);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(92, 33);
+            this.button2.Size = new System.Drawing.Size(95, 33);
             this.button2.TabIndex = 13;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = true;
@@ -166,18 +169,28 @@ namespace WF6
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(417, 316);
+            this.button3.Location = new System.Drawing.Point(328, 316);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(98, 33);
+            this.button3.Size = new System.Drawing.Size(95, 33);
             this.button3.TabIndex = 14;
             this.button3.Text = "Register";
             this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(446, 316);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 33);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Back";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // FormRegister
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(620, 401);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -216,5 +229,6 @@ namespace WF6
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
