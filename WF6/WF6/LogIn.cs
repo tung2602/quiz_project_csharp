1c678294a6cd0c2a1518beae62f6c3318532d408
tung2602 <66933495+tung2602@users.noreply.github.com>
2021-05-25 11:50:12 +0700

    update branch tung
    update branch tung

---
+++ WF6/WF6/LogIn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF6
+{
+    public partial class LogIn : Form
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+/*
+            test mp = new test();
+            mp.Text = " Welcome Mr." + textBox1.Text;
+            this.Hide();
+            mp.ShowDialog();
+            this.Close();*/
+        }
+    }
+}
